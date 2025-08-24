6213b287652dba97c46716cb911c2162684df064
xcaeser <oxidase-airbase-0m@icloud.com>
2025-08-24 12:49:03 +0000

    fix: update zli dependency to version 4.1.1 and enhance spinner functionality in start command

--- src/cli/start.zig
+++ src/cli/start.zig
@@ -17,30 +17,35 @@ pub fn register(writer: *Writer, allocator: std.mem.Allocator) !*zli.Command {
 }
 
 fn run(ctx: zli.CommandContext) !void {
-    // const writer = ctx.writer;
-
     var spinner = ctx.spinner;
+    spinner.updateStyle(.{ .frames = Spinner.SpinnerStyles.earth, .refresh_rate_ms = 150 }); // many styles available
 
-    try spinner.start("Step 1", .{});
+    // Step 1
+    try spinner.start("Step 1", .{}); // New line
     std.Thread.sleep(2000 * std.time.ns_per_ms);
 
-    try spinner.succeed("Step 1 success", .{});
+    try spinner.succeed("Step 1 success", .{}); // each start must be closed with succeed, fail, info, preserve
 
-    spinner.updateStyle(.{ .frames = Spinner.SpinnerStyles.none, .refresh_rate_ms = 80 });
-    try spinner.start("Step 2", .{});
-    std.Thread.sleep(2000 * std.time.ns_per_ms);
+    spinner.updateStyle(.{ .frames = Spinner.SpinnerStyles.weather, .refresh_rate_ms = 150 }); // many styles available
 
-    std.Thread.sleep(1000 * std.time.ns_per_ms);
-    try spinner.updateMessage("Calculating things", .{});
-    const i = work();
+    // Step 2
+    try spinner.start("Step 2", .{}); // New line
+    std.Thread.sleep(3000 * std.time.ns_per_ms);
+
+    spinner.updateStyle(.{ .frames = Spinner.SpinnerStyles.dots, .refresh_rate_ms = 150 }); // many styles available
+    try spinner.updateMessage("Step 2: Calculating things...", .{}); // update the text of step 2
+
+    const i = work(); // do some work
 
     try spinner.info("Step 2 info: {d}", .{i});
 
+    // Step 3
     try spinner.start("Step 3", .{});
     std.Thread.sleep(2000 * std.time.ns_per_ms);
+
     try spinner.fail("Step 3 fail", .{});
 
-    try spinner.print("ehe\n", .{});
+    try spinner.print("Finish\n", .{});
 }
 
 fn work() u128 {
